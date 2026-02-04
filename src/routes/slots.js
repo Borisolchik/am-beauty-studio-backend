c75cff2c0384cda34e0268b2809844641082db07
Olga Boris <olya_boris97@mail.ru>
2026-02-04 14:30:25 +0300

    fix

--- src/routes/slots.js
+++ src/routes/slots.js
@@ -2,9 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { getSlotsByMaster, createBooking } = require('../controllers/slotsController');
 
-router.post('/', createBooking);
 router.get('/:masterName', getSlotsByMaster);
-router.get('/admin/:masterId/slots', getSlotsByMasterId);
-router.patch('/toggle', toggleSlotByMaster);
+router.post('/', createBooking);
 
 module.exports = router;
